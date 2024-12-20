4613459f671458ef19a856e50a118b9801fcaadd
joakimoh <dr.joakim.ohlsson@gmail.com>
2024-12-20 20:15:19 +0100

    Add project files.

---
+++ AssemblerUtils/AssemblerUtils.cpp
@@ -0,0 +1,12 @@
+﻿// AssemblerUtils.cpp : Defines the entry point for the application.
+//
+
+#include "AssemblerUtils.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
